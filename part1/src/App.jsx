6ee38b3445ff5778e93742d8d445b6296f5b10ce
Bruh <bruh@>
2023-09-21 14:28:42 +0200

    bruh2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,25 +2,54 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+ const course = 'Half Stack application development'
+const App = () => {
+ 
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-const Hello = (props) => {
-  console.log(props)
   return (
     <div>
+      <Header/>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        <Part1/>
       </p>
+      <p>
+        <Part2/>
+      </p>
+      <p>
+        <Part3/>
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
 
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
-
+const Header = () => {
   return (
-    <div>
-      <p>{friends}</p>
-    </div>
+    <h1>{course}</h1>
+  )
+}
+
+const Part1 = () => {
+  return (
+    {part1},{exercises1}
+  )
+}
+
+const Part2 = () => {
+  return (
+    {part2},{exercises2}
+  )
+}
+
+const Part3 = () => {
+  return (
+    {part3},{exercises3}
   )
 }
 
